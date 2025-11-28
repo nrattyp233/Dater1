fd7f335096f6b72e1c96ddc8883760c201170b52
jluc9234 <dogpoundreject1@gmail.com>
2025-11-27 21:12:34 -0700

    ljkj

--- constants.tsx
+++ constants.tsx
@@ -159,6 +159,13 @@ export const CheckCircleIcon = ({ className }: { className?: string }) => (
     </svg>
 );
 
+export const PencilIcon = ({ className }: { className?: string }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M11 4H4a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7"/>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M18.5 2.5a2.121 2.121 0 013 3L12 15l-4 1 1-4 9.5-9.5z"/>
+    </svg>
+);
+
 // BADGES
 const FirstDateIcon = ({ className }: { className?: string }) => (
     <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
